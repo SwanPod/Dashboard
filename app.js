97a03bb5984988b521939cf52ea05214b0ceaeff
Ryan Carnovsky <rcarnovsky@gmail.com>
2019-03-08 11:24:26 -0500

    Create base front-end pages, including partials. Update app.js to render index

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-	res.send('/ GET Route');
+	res.render('index');
 });
 
 app.listen(8080, console.log('Started server at http://localhost:8080/'));
